91be0d8367c5200c11790fb3a1205052d45b14a6
David Bonnici <davidbonnici1984@gmail.com>
2021-04-01 19:56:58 +0200

    Implementing MVP for Hardware Mapping #2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 
+from tabulate import tabulate
 import logging
 import time
 
@@ -16,6 +17,32 @@ from adafruit_mcp230xx.mcp23017 import MCP23017
 log_init()
 logger = logging.getLogger("default")
 
+extender_bus_details = []
+extender_bus_bit_details = []
+
+
+def bus_interrupt(port):
+    _bus = None
+    _bus_instance = None
+    _bus_pins = []
+    for bus in extender_bus_details:
+        if int(bus["interrupt_pin"]) == int(port):
+            _bus = bus
+            _bus_instance = bus["bus_instance"]
+            break
+
+    for bit in extender_bus_bit_details:
+        if bit["extender_bus_id"] == _bus["extender_bus_id"]:
+            _bus_pins.append(bit)
+
+    for pin_flag in _bus_instance.int_flag:
+        for pin in _bus_pins:
+            if int(pin["bus_bit"]) == int(pin_flag):
+                if not pin["extender_bit_instance"].value:
+                    logger.warning(f"Hardware Input Selector: {pin['input_selector_name']} ===> Pressed")
+                    _bus_instance.clear_ints()
+                break
+
 
 def main():
     try:
@@ -28,6 +55,10 @@ def main():
 
         board_details = http_client.get_hardware_board_details(1)
 
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(24, GPIO.IN, GPIO.PUD_UP)
+        GPIO.setup(23, GPIO.IN, GPIO.PUD_UP)
+
         # ---------------------------------------------
         # Initialize the I2C bus:
         i2c = busio.I2C(board.SCL, board.SDA)
@@ -35,15 +66,20 @@ def main():
         # Initialize the MCP23017 chips
         extender_address_start = 32
 
-        extender_bus_details = []
-        extender_bus_bit_details = []
-
         for extender in board_details.io_extender_buses:
             logger.info(f"Registering Extender: Id:{extender.id}, {extender.name} with HEX address: "
                         f"{hex(extender_address_start)}")
             bus = MCP23017(i2c, address=extender_address_start)
-            extender_bus_details.append({"extender_bus_id": extender.id, "extender_bus_name": extender.name,
-                                         "bus_instance": bus})
+
+            extender_data_dict = {"extender_bus_id": extender.id,
+                                  "extender_bus_name": extender.name,
+                                  "bus_instance": bus,
+                                  "interrupt_pin": 24 if extender.id == 1 else 23}
+            GPIO.add_event_detect(int(extender_data_dict["interrupt_pin"]),
+                                  GPIO.FALLING,
+                                  callback=bus_interrupt,
+                                  bouncetime=200)
+            extender_bus_details.append(extender_data_dict)
 
             bus.interrupt_enable = 0xFFFF
             bus.interrupt_configuration = 0x0000  # interrupt on any change
@@ -53,35 +89,47 @@ def main():
 
             bit_counter = 0
             for extender_bit in extender.io_extender_bus_bits:
-                pin = bus.get_pin(bit_counter)
+                bus_bit = int(extender_bit.name.split("Bit")[1])
+                pin = bus.get_pin(bus_bit)
                 extender_bit_data_dict = {"extender_bus_id": extender.id,
                                           "extender_bus_name": extender.name,
                                           "bus": bus,
                                           "extender_bit_id": extender_bit.id,
                                           "extender_bit_name": extender_bit.name,
-                                          "isInput": True if extender_bit.hardware_input_selector_fullname is not None else False,
+                                          "bus_bit": bus_bit,
+                                          "is_input": True if extender_bit.hardware_input_selector_fullname is not None else False,
                                           "input_selector_name": extender_bit.hardware_input_selector_fullname,
-                                          "isOutput": True if extender_bit.hardware_output_selector_fullname is not None else False,
+                                          "is_output": True if extender_bit.hardware_output_selector_fullname is not None else False,
                                           "output_selector_name": extender_bit.hardware_output_selector_fullname,
                                           "extender_bit_instance": pin}
-                if extender_bit_data_dict["isInput"]:
+
+                # Set Default Input Mode to Output. Not doing this will cause abnormal interrupts which are caused
+                # due to the pin being in unstable state (not 0v/5v/3.3v)
+
+                if extender_bit_data_dict["is_input"] is False and extender_bit_data_dict["is_output"] is False:
+                    extender_bit_data_dict["is_output"] = True
+
+                if extender_bit_data_dict["is_input"]:
                     pin.direction = Direction.INPUT
                     pin.pull = Pull.UP
 
-                if extender_bit_data_dict["isOutput"]:
+                if extender_bit_data_dict["is_output"]:
                     pin.direction = Direction.OUTPUT
 
                 extender_bus_bit_details.append(extender_bit_data_dict)
 
                 bit_counter += 1
+
             extender_address_start += 1
 
-        # bus_instance = extender_bus_details[0]["bus_instance"]
-        # bit_instance = extender_bus_bit_details[0]["extender_bit_instance"]
-        bus_instance = extender_bus_details[0]
-        bit_instance = extender_bus_bit_details[0]
+        print(tabulate(extender_bus_details,  headers="keys", tablefmt="rst"))
+        print(tabulate(extender_bus_bit_details, headers="keys", tablefmt="rst"))
 
-        asdasd = 1
+        #for i in range(0, len(extender_bus_bit_details)):
+        #    logger.info(f"{extender_bus_details[i]['extender_bus_id']}, {extender_bus_details[i]['extender_bus_name']}")
+
+        while True:
+            time.sleep(5)
 
     except NetworkingException as ex:
         logger.critical(f"Networking Exception occurred with message {ex}")

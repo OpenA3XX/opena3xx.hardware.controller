56250154cba618544a03d55682069cb193b1dfc1
Jak Kav <71826077+Kavster89@users.noreply.github.com>
2022-01-19 15:49:59 +0000

    Update setup.sh
    git clone lline commented out as the git repo will have already been cloned. It causes fatal error on running script as directory already exists.

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,7 +3,9 @@ grep -qxF 'gpio=5=op,dh' /boot/config.txt || echo 'gpio=5=op,dh' >> /boot/config
 grep -qxF 'gpio=6=op,dh' /boot/config.txt || echo 'gpio=6=op,dh' >> /boot/config.txt
 grep -qxF 'gpio=18=op,dh' /boot/config.txt || echo 'gpio=18=op,dh' >> /boot/config.txt
 
-git clone https://github.com/OpenA3XX/opena3xx.hardware.controller.git /home/pi/opena3xx.hardware.controller
+#Remove git clone?
+#git clone https://github.com/OpenA3XX/opena3xx.hardware.controller.git /home/pi/opena3xx.hardware.controller
+
 cd /home/pi/opena3xx.hardware.controller
 pip3 install -r requirements.txt
 

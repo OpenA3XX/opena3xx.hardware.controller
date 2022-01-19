0329a9ae5b57c3fb3168cd8de2d1daa348c699e6
Jak Kav <71826077+Kavster89@users.noreply.github.com>
2022-01-19 15:53:27 +0000

    Update start.sh
    The auto update command is causing issues with install. When branch 'V1' is created and ready for release, this line can be uncommented.

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-git -C /home/pi/opena3xx.hardware.controller pull origin main
+# Remove auto-update and replace with releases. Will add line back in when V1 ready for release. Branch 'V1' to be created for release.
+#git -C /home/pi/opena3xx.hardware.controller pull origin V1
+
 pip3 install -r /home/pi/opena3xx.hardware.controller/requirements.txt
 python3 /home/pi/opena3xx.hardware.controller/main.py --hardware-board-id=6

65a753ffd3b3ee8daec200342bdbe3d9183324a1
Jak Kav <71826077+Kavster89@users.noreply.github.com>
2022-01-20 14:30:20 +0000

    Update ip-test.sh

--- setup/ip-test.sh
+++ setup/ip-test.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-ip addr show wlan0 | grep -Po 'inet \K[\d.]+'
+#ip addr show wlan0 | grep -Po 'inet \K[\d.]+'
 
 # Backup command
 #ip -f inet addr show wlan0 | grep -Po 'inet \K[\d.]+'
+
+#sed -e 's/\("opena3xx-peripheral-api-ip"\:\)\("\)\(value1\)/\1New\2/g' filename
+#sed -e 's/\(key1\=\)\(Old\)\(value1\)/\1New\2/g' filename
+
+ipAddr=$(#ip -f inet addr show wlan0 | grep -Po 'inet \K[\d.]+)
+
+sed -c -i "s/\("opena3xx-peripheral-api-ip" *: *\).*/\1$ipAddr/" /home/pi/opena3xx.hardware.controller/configuration/configuration.json
+
+#sh -c "grep -qxF '"opena3xx-peripheral-api-ip": ' /home/pi/opena3xx.hardware.controller/configuration/configuration.json || echo | ip addr show wlan0 | grep -Po 'inet \K[\d.]+' >> /boot/config.txt"

869a6f0d9fdaeb34bbc1ee98e2d13fc807edc791
Jak Kav <71826077+Kavster89@users.noreply.github.com>
2022-01-20 19:55:55 +0000

    Create progress.sh

---
+++ setup/progress.sh
@@ -0,0 +1,19 @@
+phases=( 
+    'Checking in to Terminal'
+    'Weighing lugagge'
+    'Security & Passport control'
+    'Now boarding'
+)   
+
+for i in $(seq 1 100); do  
+    sleep 0.1
+
+    if [ $i -eq 100 ]; then
+        echo -e "XXX\n100\nReady for takeoff!\nXXX"
+    elif [ $(($i % 25)) -eq 0 ]; then
+        let "phase = $i / 25"
+        echo -e "XXX\n$i\n${phases[phase]}\nXXX"
+    else
+        echo $i
+    fi 
+done | whiptail --title 'OpenA3XX Hardware Controller' --gauge "${phases[0]}" 6 60 0

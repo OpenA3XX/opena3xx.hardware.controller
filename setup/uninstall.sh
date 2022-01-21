bb4de0b183b88c72b25962d30f984ab66e874999
Jak Kav <71826077+Kavster89@users.noreply.github.com>
2022-01-21 01:36:31 +0000

    Update uninstall.sh

--- setup/uninstall.sh
+++ setup/uninstall.sh
@@ -7,48 +7,48 @@ NC='\033[0m'
 
 sudo systemctl stop opena3xx-hardware-controller.service
 if [ $? -eq 0 ]; then
-   echo "Automatic start-up service stopped" ${GREEN} $'\u2713\n' ${NC}
+   echo "${GREEN}Automatic start-up service stopped${NC}"
 else
-   echo "Failed to stop /lib/systemd/system/opena3xx-hardware-controller.service" ${RED} $'\u2717\n' ${GREEN}
+   echo "Failed to stop /lib/systemd/system/opena3xx-hardware-controller.service"
    failed=true
 fi
 
 sudo systemctl disable opena3xx-hardware-controller.service
 if [ $? -eq 0 ]; then
-   echo "Automatic start-up service disabled" ${GREEN} $'\u2713\n' ${NC}
+   echo "${GREEN}Automatic start-up service disabled${NC}"
 else
-   echo "Failed to disable /lib/systemd/system/opena3xx-hardware-controller.service" ${RED} $'\u2717\n' ${GREEN}
+   echo "Failed to disable /lib/systemd/system/opena3xx-hardware-controller.service"
    failed=true
 fi
 
 sudo rm -rf opena3xx.hardware.controller
 if [ $? -eq 0 ]; then
-   echo "opena3xx.hardware.controller directory removed" ${GREEN} $'\u2713\n' ${NC}
+   echo "${GREEN}opena3xx.hardware.controller directory removed${NC}"
 else
-   echo "Failed to stop /lib/systemd/system/opena3xx-hardware-controller.service" ${RED} $'\u2717\n' ${GREEN}
+   echo "Failed to stop /lib/systemd/system/opena3xx-hardware-controller.service"
    failed=true
 fi
 
 sudo rm /lib/systemd/system/opena3xx-hardware-controller.service
 if [ $? -eq 0 ]; then
-   echo ${GREEN} $'\u2713\n' ${NC}
+   echo ""
 else
-   echo "Failed to stop /lib/systemd/system/opena3xx-hardware-controller.service" ${RED} $'\u2717\n' ${GREEN}
+   echo "Failed to stop /lib/systemd/system/opena3xx-hardware-controller.service"
    failed=true
 fi
 
 sudo systemctl daemon-reload
 if [ $? -eq 0 ]; then
-   echo "Daemon reloaded" $'\u2713\n'
+   echo "${GREEN}Daemon reloaded${NC}"
 else
-   echo "Failed to reload daemon" ${RED} $'\u2717\n' ${GREEN}
+   echo "Failed to reload daemon"
    failed=true
 fi
 
-if [ $failed -eq 1 ]
+if [ $failed -eq true ]
 then
     echo "There was an ${RED}error${NC} with the uninstall process. Please check the error message above."
     echo "If unable to manually resolve the problem, please contact a member of the OpenA3XX team."
 else
-    echo "OpenA3XX Hardware Controller uninstall ${GREEN}successful${NC}"
+    echo "${GREEN}OpenA3XX Hardware Controller uninstall successful${NC}"
 fi

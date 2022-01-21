a2a48626dd421d24f5b950bd851664f8374b3521
Jak Kav <71826077+Kavster89@users.noreply.github.com>
2022-01-21 15:01:43 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 # Remove auto-update and replace with releases. Will add line back in when V1 ready for release. Branch 'V1' to be created for release.
 #git -C /home/pi/opena3xx.hardware.controller pull origin V1
 
-pip3 install -r /home/pi/opena3xx.hardware.controller/requirements.txt
-#python3 /home/pi/opena3xx.hardware.controller/main.py --hardware-board-id=
+# Removed auto library update for now. Enables faster start, and library update not essential at this stage.
+#pip3 install -r /home/pi/opena3xx.hardware.controller/requirements.txt
+
+# This line is the program start. It has been commented out, as the installer will insert a new line below with the relevant board ID number from the installer.
+#python3 /home/pi/opena3xx.hardware.controller/main.py --hardware-board-id=6

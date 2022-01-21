576fc2caf0f34cbafcdc5c8aee0f4791d69bb91b
Jak Kav <71826077+Kavster89@users.noreply.github.com>
2022-01-21 16:29:37 +0000

    Final V1
    Had to relocate the spinner as it interfered with the user input.

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,24 +8,6 @@ NC='\033[0m'
 # Start the installer
 echo -e "${GREEN}Welcome to OpenA3XX! I'm just installing some required packages, please wait.${NC}"
 
-# Create and start the loading spinner
-spin()
-{
-  spinner="/|\\-/|\\-"
-  while :
-  do
-    for i in `seq 0 7`
-    do
-      echo -n "${spinner:$i:1}"
-      echo -en "\010"
-      sleep 1
-    done
-  done
-}
-spin &
-SPIN_PID=$!
-trap "kill -9 $SPIN_PID" `seq 0 15`
-
 # Print logo to terminal
 sudo apt install figlet &> /dev/null
 sudo apt install lolcat &> /dev/null
@@ -62,8 +44,26 @@ done
 echo -e "${GREEN}Thanks! I'll make this board number ${BLUE} $boardIDNum ${GREEN} ! ${NC}"
 echo "python3 /home/pi/opena3xx.hardware.controller/main.py --hardware-board-id=$boardIDNum" >> /home/pi/opena3xx.hardware.controller/start.sh
 
-# Install the required libraries
+# Create and start the loading spinner
+spin()
+{
+  spinner="/|\\-/|\\-"
+  while :
+  do
+    for i in `seq 0 7`
+    do
+      echo -n "${spinner:$i:1}"
+      echo -en "\010"
+      sleep 1
+    done
+  done
+}
 echo -e "${GREEN}Checking for updates, ${RED}please wait!${GREEN} This may take some time, especially on first install.${NC}"
+spin &
+SPIN_PID=$!
+trap "kill -9 $SPIN_PID" `seq 0 15`
+
+# Install the required libraries
 pip3 install -r requirements.txt
 
 # Remove any existing version of the start up service (if it exists)
